b25abd9be9a75eb3bfdc0a20b6a77d4d2b3333f3
llcheng94 <llcheng94@163.com>
2019-01-08 22:19:02 +0800

    Create 103.binary-tree-zigzag-level-order-traversal.cpp

---
+++ algorithm/103.binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=103 lang=cpp
+ *
+ * [103] Binary Tree Zigzag Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (39.69%)
+ * Total Accepted:    183.4K
+ * Total Submissions: 462.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the zigzag level order traversal of its nodes'
+ * values. (ie, from left to right, then right to left for the next level and
+ * alternate between).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its zigzag level order traversal as:
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [20,9],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(root == NULL)
+            return result;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        vector<int> level;
+        bool order = false;
+
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            if(node != NULL){
+                level.push_back(node->val);
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            else{
+                if(order)
+                    reverse(begin(level), end(level));
+                order = !order;
+                result.push_back(level);
+                level.clear();
+                if(!q.empty()){
+                    q.push(NULL); 
+                }
+            }
+        }
+
+        return result;
+    }
+};
