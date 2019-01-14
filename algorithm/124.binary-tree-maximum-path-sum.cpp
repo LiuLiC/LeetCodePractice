ee5ac98f348914a9e39e048afcf20d4280a7ae06
llcheng94 <llcheng94@163.com>
2019-01-14 21:46:35 +0800

    Create 124.binary-tree-maximum-path-sum.cpp

---
+++ algorithm/124.binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=124 lang=cpp
+ *
+ * [124] Binary Tree Maximum Path Sum
+ *
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (28.90%)
+ * Total Accepted:    164.6K
+ * Total Submissions: 569.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a non-empty binary tree, find the maximum path sum.
+ * 
+ * For this problem, a path is defined as any sequence of nodes from some
+ * starting node to any node in the tree along the parent-child connections.
+ * The path must contain at least one node and does not need to go through the
+ * root.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3]
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   3
+ * 
+ * Output: 6
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [-10,9,20,null,null,15,7]
+ * 
+ * -10
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 
+ * Output: 42
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    int max_sum;
+public:
+    int nodeSum(TreeNode* node){
+        if(node == NULL)
+            return 0;
+        
+        int left = max(0, nodeSum(node->left));
+        int right = max(0, nodeSum(node->right));
+        max_sum = max(max_sum, left + right + node->val);
+
+        return max(left, right) + node->val;
+    }
+
+    int maxPathSum(TreeNode* root) {
+        max_sum = INT_MIN;
+        nodeSum(root);
+
+        return max_sum;
+    }
+};
