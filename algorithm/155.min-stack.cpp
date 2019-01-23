4931afb9c878c37b98ada7093efadd6badbf808b
llcheng94 <llcheng94@163.com>
2019-01-23 20:54:46 +0800

    Create 155.min-stack.cpp
    [TODO]Use one stack to solve this problem.

---
+++ algorithm/155.min-stack.cpp
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=155 lang=cpp
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (34.97%)
+ * Total Accepted:    256.2K
+ * Total Submissions: 732K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * 
+ * 
+ * pop() -- Removes the element on top of the stack.
+ * 
+ * 
+ * top() -- Get the top element.
+ * 
+ * 
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ */
+class MinStack {
+private:
+    int min_value;
+    stack<int> s;
+    stack<int> min_s;
+public:
+    /** initialize your data structure here. */
+    MinStack() : min_value(INT_MAX) { }
+    
+    void push(int x) {
+        s.push(x);
+        
+        if(min_s.empty()){
+            min_s.push(x);
+            min_value = x;
+        }
+        else{
+            min_value = min_s.top();
+
+            if(min_value <= x){
+                min_s.push(min_value);
+            }
+            else{
+                min_value = x;
+                min_s.push(x);
+            }
+        }
+    }
+    
+    void pop() {
+        s.pop();
+        min_s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return min_s.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
